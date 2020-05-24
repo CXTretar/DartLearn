c210736d9dbbe0b097f9f78ba870cae663a50c62
CXTretar <misscxuan@163.com>
2020-05-24 23:34:28 +0800

    增加注释。

--- 05_Dart的库的使用/01_使用系统的库.dart
+++ 05_Dart的库的使用/01_使用系统的库.dart
@@ -1,12 +1,9 @@
-
 import 'dart:math';
 
 main(List<String> args) {
+  // List/Map : 因为在 dart:core 中,可以直接使用. 其他的库类似 dart:io 需要特别去导入
 
-    // List/Map : 因为在 dart:core 中,可以直接使用. 其他的库类似 dart:io 需要特别去导入
-
-    final num1 = 20;
-    final num2 = 30;
-    print(min(num1, num2));
-
+  final num1 = 20;
+  final num2 = 30;
+  print(min(num1, num2));
 }
